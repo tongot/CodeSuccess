3c60020c686ebe404ae63970417d15b388b0ffd5
tangani <tanganimoyo@gmail.com>
2020-09-14 23:53:21 +0200

    Setting up project folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { IndexComponent } from "./components/index/index.component";
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { AboutComponent } from './components/about/about.component';
+import { AnimationsComponent } from './components/animations/animations.component';
+import { CareersComponent } from './components/careers/careers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    LoginComponent,
+    SignupComponent,
+    CheckoutComponent,
+    CartComponent,
+    FavoritesComponent,
+    AboutComponent,
+    AnimationsComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
