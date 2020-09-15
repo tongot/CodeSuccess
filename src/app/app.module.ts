5b719f0c4ed1de37fddf1f2bfdd679bb90a15436
tangani <tanganimoyo@gmail.com>
2020-09-15 09:54:40 +0200

    Revert components in unauthorised directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { IndexComponent } from './index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    IndexComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
