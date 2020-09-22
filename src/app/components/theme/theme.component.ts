79427e1ed693e9ab8c4c8d165eed54b61fe67a84
tangani <tanganimoyo@gmail.com>
2020-09-22 16:46:22 +0200

    using Angular Material and Flexlayout for layout and styling

--- src/app/components/theme/theme.component.ts
+++ src/app/components/theme/theme.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-
 @Component({
   selector: 'app-theme',
   templateUrl: './theme.component.html',
@@ -8,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ThemeComponent implements OnInit {
 
+  /*
   slides = [
     { src: "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg" },
     { src: "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg" },
@@ -17,6 +17,16 @@ export class ThemeComponent implements OnInit {
     { src: "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(39).jpg" },
     { src: "https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(40).jpg" }
   ];
+   */
+
+  slides = [
+    { src: "/assets/carousel/new_shop_local_lp_carousel_1292x300_2020_1.png" },
+    { src: "/assets/carousel/new_shop_local_lp_carousel_1292x300_2020_1.png" },
+    { src: "/assets/carousel/new_shop_local_lp_carousel_1292x300_2020_1.png" },
+    { src: "/assets/carousel/new_shop_local_lp_carousel_1292x300_2020_1.png" }
+  ];
+
+
 
   brands = [
     { src: '/assets/carousel/brands/berlingerhaus.webp'},
@@ -31,6 +41,14 @@ export class ThemeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  myFunction(index) {
+    window.location.assign("https://www.google.com");
+  }
+
+  public onCardClick(evt: MouseEvent){
+    window.location.assign("https://www.google.com");
   }
 
 
