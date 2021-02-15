acdebac383410159ff440d1649711c6c4611e209
Hlanga <52770619+Hlanga@users.noreply.github.com>
2021-02-15 11:09:01 +0200

    Update product-service.ts

--- src/app/Services/product-service.ts
+++ src/app/Services/product-service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, of, throwError, Subject } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators';
 import { IProduct } from '../shared/product';
 
@@ -9,83 +9,79 @@ import { IProduct } from '../shared/product';
   providedIn: 'root'
 })
 export class ProductService {
-  private dataUrl = 'assets/api/products.json';
-  length: any;
 
   constructor(private http: HttpClient, private route: Router) { }
+  private dataUrl = 'assets/data';
+  products: IProduct[];
 
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.dataUrl)
       .pipe(
-        tap(data => console.log(`All   products: ${JSON.stringify(data)}`))
+        tap(data => console.log(JSON.stringify(data))),
+        catchError(this.handleError)
       );
   }
-  getProduct(id: string): Observable<IProduct> {
-    if (id === '0') {
+
+  getProduct(id: number): Observable<IProduct> {
+    if (id === 0) {
       return of(this.initializeProduct());
     }
-    return this.http.get<IProduct>(this.dataUrl)
+    const url = `${this.dataUrl}/${id}`;
+    return this.http.get<IProduct>(url)
       .pipe(
-        tap(data => {return data[3]}),
+        tap(data => console.log('getProduct: ' + JSON.stringify(data))),
         catchError(this.handleError)
       );
   }
 
   createProduct(product: IProduct): Observable<IProduct> {
-    const headers = new HttpHeaders({ 'Content-Type': 'app/json' });
     product.id = null;
-    return this.http.post<IProduct>(this.dataUrl, product, { headers })
+    return this.http.post<IProduct>(this.dataUrl, product)
       .pipe(
         tap(data => console.log('createProduct: ' + JSON.stringify(data))),
         catchError(this.handleError)
       );
   }
 
-  deleteProduct(id: string): Observable<{}> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  deleteProduct(id: number): Observable<{}> {
     const url = `${this.dataUrl}/${id}`;
-    return this.http.delete<IProduct>(url, { headers })
+    return this.http.delete<IProduct>(url)
       .pipe(
         tap(data => console.log('deleteProduct: ' + id)),
         catchError(this.handleError)
       );
-
   }
 
   updateProduct(product: IProduct): Observable<IProduct> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const url = `${this.dataUrl}/${product.id}`;
-    return this.http.put<IProduct>(url, product, { headers })
+    return this.http.put<IProduct>(url, product)
       .pipe(
-        tap(() => console.log('Product: ' + product.id + 'has been updated' )),
-        // Return the product on an update
+        tap(() => console.log('updateProduct: ' + product.id)),
+        // Return the Product on an update
         map(() => product),
         catchError(this.handleError)
       );
   }
 
   // tslint:disable-next-line: typedef
-  private handleError(err: { error: {
-    message: any; };
-     status: any; body: { error: any;
-     };
-    }) {
+  private handleError(err: any) {
     let errorMessage: string;
     if (err.error instanceof ErrorEvent) {
-      // A client-side error occurred
+      // A client-side or network error occurred. Handle it accordingly.
       errorMessage = `An error occurred: ${err.error.message}`;
     } else {
       // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
       errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
     }
-    console.error(err);
+    // console.error(err);
     return throwError(errorMessage);
   }
 
 
   private initializeProduct(): IProduct {
     return {
-      id: null,
+      id: 0,
       name: null,
       image: null,
       department: null,
@@ -94,7 +90,11 @@ export class ProductService {
       price: null,
       discount: null,
       category: null,
-      rating: null
+      rating: null,
+      tags: []
     };
   }
+
+
+ 
 }
